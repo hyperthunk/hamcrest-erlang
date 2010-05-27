1a4296b71cada84074540205fb386656506b192b
Tim Watson <watson.timothy@gmail.com>
2010-05-27 09:03:32 +0100

    updating licence and makefile

--- src/hamcrest_matchers.erl
+++ src/hamcrest_matchers.erl
@@ -1,5 +1,33 @@
+%% -----------------------------------------------------------------------------
+%%
+%% Hamcrest Erlang.
+%%
+%% Copyright (c) 2010 Tim Watson (watson.timothy@gmail.com)
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+%% -----------------------------------------------------------------------------
 %% @author Tim Watson <watson.timothy@gmail.com>
-%% @copyright 2008 Tim Watson.
+%% @copyright 2010 Tim Watson.
+%% @doc Hamcrest Matchers
+%% @reference See <a href="http://code.google.com/p/hamcrest/">Hamcrest</a>
+%% for more information.
+%% -----------------------------------------------------------------------------
 
 -module(hamcrest_matchers).
 -author('Tim Watson <watson.timothy@gmail.com>').
@@ -23,6 +51,7 @@
 anything() ->
     fun(_) -> true end.
 
+-spec(any_of/1 :: (list(fun((term()) -> boolean()))) -> fun((term()) -> boolean())).
 any_of(Matchers) when is_list(Matchers) ->
     fun(X) -> lists:member(true, [ M(X) || M <- Matchers ]) end.
 
